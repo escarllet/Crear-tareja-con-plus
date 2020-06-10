142c7a6f585fbc0b7daa824ed7e86fe932c5985c
Escarllet Suriel Valdez <Escarlletsurielvaldez@hotmail.com>
2020-06-09 20:57:02 -0500

    Identifica si es letra, si es signo de puntuacion o numero. version 1.0

---
+++ Identifica el caracter/Identifica el caracter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identifica_el_caracter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            char b,caracter;
+
+            Console.WriteLine("Ingrese un caracter o numero");
+            string a = Console.ReadLine();
+
+            bool isNumeric = int.TryParse(a, out n);
+            bool isChar = char.TryParse(a, out b);
+            caracter = Convert.ToChar(a);
+
+            if (isNumeric == true)
+            {
+                Console.WriteLine("es un numero ");
+            }
+            else if (isChar == true)
+            {
+                if ((caracter == '.') || (caracter == ',') || (caracter == ';') || caracter == ':' || 
+                    (caracter == '¿') || (caracter == '?') || (caracter == '!') || (caracter == '¡'))
+                {
+                    Console.WriteLine("es signo de puntuacion ");
+                }
+                else
+                {
+                     Console.WriteLine("es una letra ");
+                }
+
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
+       
