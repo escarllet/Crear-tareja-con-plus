ac058df8a0520b6193f96cfa6f4c210cbc51ab78
Escarllet Suriel Valdez <Escarlletsurielvaldez@hotmail.com>
2020-06-09 19:31:20 -0500

    Mostrar el valor absotuto de un numero. version 1.0

---
+++ Mostrar el valor absoluto/Mostrar el valor absoluto/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mostrar_el_valor_absoluto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Ingrese un numero para obtener su valor absoluto");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a >= 0)
+            {
+                Console.WriteLine("El valor absoluto del numero " + a + " es: " + a);
+            }
+            else
+            {
+                b = a * -1;
+                Console.WriteLine("El valor absoluto del numero "+ a + " es: "+b);
+            }
+            Console.ReadKey();
+        }
+    }
+}
