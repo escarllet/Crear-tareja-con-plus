662b08e44af566613af9db3ad082111eb88e0353
Escarllet Suriel Valdez <Escarlletsurielvaldez@hotmail.com>
2020-06-09 19:38:35 -0500

    Mayor de dos numeros. version 1.0

---
+++ Menor de dos numeros/Menor de dos numeros/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menor_de_dos_numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Ingrese un numero");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero");
+            b = Convert.ToInt32(Console.ReadLine());
+            if (a>b)
+            {
+                Console.WriteLine(a + " es mayor");
+            }
+            else if (b>a)
+            {
+                Console.WriteLine(b + " es mayor");
+            }
+            else
+            {
+                Console.WriteLine("Los numeros son iguales");
+            }
+            Console.ReadKey();
+        }
+    }
+}
