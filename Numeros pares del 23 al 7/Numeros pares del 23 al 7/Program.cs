e978eb6c2b71a790333b9e128632f61d38aee0e1
Escarllet Suriel Valdez <Escarlletsurielvaldez@hotmail.com>
2020-06-09 22:39:53 -0500

    Numeros pares del 23 al 7 usando while. version 1.0

---
+++ Numeros pares del 23 al 7/Numeros pares del 23 al 7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeros_pares_del_23_al_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 23;
+
+            while (a>=7)
+            {
+                if (a%2==0)
+                {
+                    Console.WriteLine(a);
+                }
+                if (a<=7)
+                {
+                    break;
+                }
+                a -= 1;
+            }
+        }
+    }
+}
